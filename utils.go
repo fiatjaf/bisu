505a09bf1a4e5b8d3c116baff4d404bea657da18
fiatjaf <fiatjaf@gmail.com>
2023-08-14 15:00:15 -0300

    remove warnings.

--- utils.go
+++ utils.go
@@ -14,12 +14,10 @@ func shortUint64(idOrPubkey string) uint64 {
 	length := len(idOrPubkey)
 	if length < 8 {
 		panic("shortUint64 called with bad value")
-		return 0
 	}
 	b, err := hex.DecodeString(idOrPubkey[length-8:])
 	if err != nil {
 		panic(err)
-		return 0
 	}
 	return uint64(binary.BigEndian.Uint32(b))
 }
